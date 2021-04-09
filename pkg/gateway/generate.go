b74feff1000da78e8b573960b75fe9964ebdac3f
Qingyu Wu <soupwu@tencent.com>
2021-04-09 10:58:16 +0800

    feat(gateway): change dev tag to 2.5.0
    Signed-off-by: Qingyu Wu <soupwu@tencent.com>

--- pkg/gateway/generate.go
+++ pkg/gateway/generate.go
@@ -32,9 +32,10 @@ import (
 )
 
 const (
-	defaultImage              = "ccr.ccs.tencentyun.com/kubeflow-oteam/enterprise-gateway:dev"
-	defaultContainerName      = "gateway"
-	defaultKernelImage        = "ccr.ccs.tencentyun.com/kubeflow-oteam/jupyter-kernel-py:dev"
+	defaultImage         = "ccr.ccs.tencentyun.com/kubeflow-oteam/enterprise-gateway:2.5.0"
+	defaultContainerName = "gateway"
+	// TODO: This default kernel is not used by now
+	defaultKernelImage        = "ccr.ccs.tencentyun.com/kubeflow-oteam/jupyter-kernel-py:2.5.0"
 	defaultPortName           = "gateway"
 	defaultKernel             = "python_kubernetes"
 	defaultPort               = 8888
